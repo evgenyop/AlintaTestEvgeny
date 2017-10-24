0fa59126284e70e7359df82a6c13c664e445651e
Evgeny Petrov <evgeny.petrov@revium.local>
2017-10-24 15:00:40 +1100

    Add project files.

---
+++ AlintaTestEvgeny/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Rest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using AlintaTestEvgeny.Models.Movie;
+using AlintaTestEvgeny.RESTful;
+
+namespace AlintaTestEvgeny.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var model = MovieCharacter.GetList();
+
+            return View(model);
+        }
+    }
+}
---
+++ AlintaTestEvgeny/Models/Movie/MovieCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Rest;
+
+using AlintaTestEvgeny.RESTful;
+using System.Configuration;
+
+namespace AlintaTestEvgeny.Models.Movie
+{
+    public class MovieCharacter
+    {
+        public string CharacterName { get; set; }
+        public string ActorName { get; set; }
+        public string MovieName { get; set; }
+
+        internal static List<MovieCharacter> GetList()
+        {
+            var credentials = new BasicAuthenticationCredentials();
+            var uri = new Uri(ConfigurationManager.AppSettings["MoviesService.BaseURL"]);
+            var restClient = new RESTfulClient(uri, credentials);
+            var list = restClient.ApiMoviesGet().SelectMany(m => m.Roles, (m, r) => new MovieCharacter
+            {
+                MovieName = m.Name,
+                ActorName = r.Actor,
+                CharacterName = r.Name
+            }).
+            OrderBy(r => r.ActorName).ThenBy(r => r.MovieName).ToList();
+
+            return list;
+        }
+    }
+}
